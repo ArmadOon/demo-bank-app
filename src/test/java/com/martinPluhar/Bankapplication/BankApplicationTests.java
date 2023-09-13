eef72327d05ce9435b8eec0f1d91dec66b960e1b
ArmadOon <pluhar.martin@outlook.com>
2023-09-13 08:27:51 +0200

    Project initialization, Employee class created

---
+++ src/test/java/com/martinPluhar/Bankapplication/BankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martinPluhar.Bankapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
