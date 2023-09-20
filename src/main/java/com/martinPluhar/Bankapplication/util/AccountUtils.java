e6321dd0b39c28b2a4ad34d54036abf35386d0b5
Martin Pluhar <83763364+ArmadOon@users.noreply.github.com>
2023-09-20 22:11:24 +0200

    AccountUtils messages rewrited to czech language

--- src/main/java/com/martinPluhar/Bankapplication/util/AccountUtils.java
+++ src/main/java/com/martinPluhar/Bankapplication/util/AccountUtils.java
@@ -4,22 +4,22 @@ import java.time.Year;
 
 public class AccountUtils {
     public static final String ACCOUNT_EXIST_CODE = "001";
-    public static final String ACCOUNT_EXIST_MESSAGE = "This user account already exist!";
+    public static final String ACCOUNT_EXIST_MESSAGE = "Tento účet již existuje!";
 
     public static final String ACCOUNT_CREATION_SUCCESS ="002";
-    public static final String ACCOUNT_CREATION_MESSAGE = "Account has been successfully created!";
+    public static final String ACCOUNT_CREATION_MESSAGE = "Váš účet byl v pořádku vytvořen!";
     public static final String ACCOUNT_NOT_EXIST_CODE = "003";
-    public static final String ACCOUNT_NOT_EXIST_MESSAGE = "User with the provided Account Number does not exist";
+    public static final String ACCOUNT_NOT_EXIST_MESSAGE = "Uživatel s požadovaným číslem účtu neexistuje!";
     public static final String ACCOUNT_FOUND_CODE = "004";
-    public static final String ACCOUNT_FOUND_SUCCESS = "User Account Found";
+    public static final String ACCOUNT_FOUND_SUCCESS = "Uživatel účtu nalezen!";
     public static final String ACCOUNT_CREDITED_SUCCESS = "005";
-    public static final String ACCOUNT_CREDITED_SUCCESS_MESSAGE = "User Account was credited successfully";
+    public static final String ACCOUNT_CREDITED_SUCCESS_MESSAGE = "Uživatelský účet byl v pořádku načerpán!";
     public static final String INSUFFICIENT_BALANCE_CODE = "006";
-    public static final String INSUFFICIENT_BALANCE_MESSAGE = "Insufficient Balance";
+    public static final String INSUFFICIENT_BALANCE_MESSAGE = "Nedostatečný zůstatek!";
     public static final String ACCOUNT_DEBITED_SUCCESS = "007";
-    public static final String ACCOUNT_DEBITED_MESSAGE = "Account has been successfully debited";
+    public static final String ACCOUNT_DEBITED_MESSAGE = "Účet byl v pořádku debitován!";
     public static final String TRANSFER_SUCCESSFUL_CODE = "008";
-    public static final String TRANSFER_SUCCESSFUL_MESSAGE = "Transfer Successful";
+    public static final String TRANSFER_SUCCESSFUL_MESSAGE = "Převod proběhl úspěsně!";
     public static String generateAccountNumber() {
 
         /*
