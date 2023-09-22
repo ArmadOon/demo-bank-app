340deb34cf7041546a7d6b22ab21e6655949c451
Martin Pluhar <83763364+ArmadOon@users.noreply.github.com>
2023-09-22 14:31:09 +0200

    OpenApiDEfiniton info added

--- src/main/java/com/martinPluhar/Bankapplication/BankApplication.java
+++ src/main/java/com/martinPluhar/Bankapplication/BankApplication.java
@@ -1,8 +1,12 @@
 package com.martinPluhar.Bankapplication;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
@@ -10,7 +14,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 		info = @Info(
 				title = "Spring-boot Bank project",
 				description = "Project for job apply in KB bank CZ",
-				version = "v1.0.0"
+				version = "v1.0.0",
+				contact = @Contact(
+						name = "Martin Pluhař",
+						email = "pluhar.martin@outlook.com",
+						url =  "https://github.com/ArmadOon/demo-bank-app"
+				),
+				license = @License(
+						name = "Martin Pluhař",
+						url =  "https://github.com/ArmadOon/demo-bank-app"
+				)
+
+
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Bank application for apply job to KB bank",
+				url =  "https://github.com/ArmadOon/demo-bank-app"
 		)
 )
 public class BankApplication {
