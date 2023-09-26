2755e583402632d173bd8c9e61216dbe4479365c
ArmadOon <pluhar.martin@outlook.com>
2023-09-26 13:53:32 +0200

    Bank statement class created

---
+++ src/main/java/com/martinPluhar/Bankapplication/services/impl/BankStatement.java
@@ -0,0 +1,18 @@
+package com.martinPluhar.Bankapplication.services.impl;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@NoArgsConstructor
+@AllArgsConstructor
+public class BankStatement {
+    /**
+     * retrieve list of transactions within a date range given an account number
+     * generate a pdf file of transactions
+     * send file via email
+     */
+
+
+}
