20feeff49212f039deb08b016c87deddf361e0e8
ArmadOon <pluhar.martin@outlook.com>
2023-10-05 08:00:43 +0200

    Documentation created for EmailServiceImpl

--- src/main/java/com/martinPluhar/Bankapplication/services/impl/EmailServiceImpl.java
+++ src/main/java/com/martinPluhar/Bankapplication/services/impl/EmailServiceImpl.java
@@ -15,15 +15,24 @@ import org.springframework.stereotype.Service;
 
 import java.io.File;
 
+/**
+ * Implementace rozhraní {@link EmailService} pro odesílání e-mailů pomocí JavaMailSender.
+ */
 @Service
 public class EmailServiceImpl implements EmailService {
     @Autowired
     private JavaMailSender javaMailSender;
 
+    // Získání e-mailové adresy odesílatele z konfigurace Spring Boot.
     @Getter
     @Value("${spring.mail.username}")
     private String senderEmail;
 
+    /**
+     * Metoda pro odeslání e-mailového upozornění s jednoduchým textovým obsahem.
+     *
+     * @param emailDetails Objekt obsahující informace o e-mailu (příjemce, předmět, tělo zprávy).
+     */
     @Override
     public void sendEmailAlert(EmailDetails emailDetails) {
         try {
@@ -33,13 +42,19 @@ public class EmailServiceImpl implements EmailService {
             mailMessage.setText(emailDetails.getMessageBody());
             mailMessage.setSubject(emailDetails.getSubject());
 
+            // Odeslání e-mailu
             javaMailSender.send(mailMessage);
             System.out.println("Email byl v pořádku odeslán!");
         } catch (MailException e) {
             throw new RuntimeException(e);
         }
-}
+    }
 
+    /**
+     * Metoda pro odeslání e-mailu s přílohou.
+     *
+     * @param emailDetails Objekt obsahující informace o e-mailu (příjemce, předmět, tělo zprávy, příloha).
+     */
     @Override
     public void sendEmailWithAttachment(EmailDetails emailDetails) {
         try {
@@ -55,6 +70,7 @@ public class EmailServiceImpl implements EmailService {
             FileSystemResource file = new FileSystemResource(new File(emailDetails.getAttachment()));
             helper.addAttachment("MyStatement.pdf", file);
 
+            // Odeslání e-mailu s přílohou
             javaMailSender.send(message);
             System.out.println("E-mail s přílohou byl odeslán!");
         } catch (Exception e) {
