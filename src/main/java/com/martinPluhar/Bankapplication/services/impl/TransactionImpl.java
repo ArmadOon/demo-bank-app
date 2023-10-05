c00c32fc09d8d3afd60f72af5fec5715f734393f
ArmadOon <pluhar.martin@outlook.com>
2023-10-05 08:03:40 +0200

    documentation created for TransactionImpl

--- src/main/java/com/martinPluhar/Bankapplication/services/impl/TransactionImpl.java
+++ src/main/java/com/martinPluhar/Bankapplication/services/impl/TransactionImpl.java
@@ -11,30 +11,41 @@ import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+/**
+ * Implementace rozhraní {@link TransactionService} pro ukládání transakcí do databáze.
+ * Tato třída zajišťuje ukládání informací o transakcích, včetně typu transakce, částky, odesílatele,
+ * příjemce a stavu transakce (zda byla úspěšně provedena).
+ */
 @Component
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-
 public class TransactionImpl implements TransactionService {
 
     @Autowired
     private TransactionRepository transactionRepository;
 
+    /**
+     * Metoda pro uložení informací o transakci do databáze.
+     *
+     * @param transactionDto  Objekt obsahující informace o transakci (typ, částka).
+     * @param senderAccount   Číslo účtu odesílatele transakce.
+     * @param receiverAccount Číslo účtu příjemce transakce.
+     */
     @Override
     public void saveTransaction(TransactionDto transactionDto, String senderAccount, String receiverAccount) {
-
+        // Vytvoření objektu transakce s potřebnými informacemi
         Transaction transaction = Transaction.builder()
                 .transactionType(transactionDto.getTransactionType())
                 .amount(transactionDto.getAmount())
                 .senderAccount(senderAccount)
                 .receiverAccount(receiverAccount)
-                .status("SUCCESS")
+                .status("SUCESS") // Předpokládáme, že transakce byla úspěšně provedena
                 .build();
+
+        // Uložení transakce do databáze
         transactionRepository.save(transaction);
-
-
     }
 }
 
