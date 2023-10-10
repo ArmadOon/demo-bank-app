9441291c8d1b599ef3f7237795de039c8d701b97
ArmadOon <pluhar.martin@outlook.com>
2023-10-10 20:33:20 +0200

    SecurityConfigCreated

---
+++ src/main/java/com/martinPluhar/Bankapplication/config/SecurityConfig.java
@@ -0,0 +1,17 @@
+package com.martinPluhar.Bankapplication.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
